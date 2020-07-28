452c59b64fee88fc9ece553f96a7ebbd8b0d2df7
MM00619560 <mm00619560@techmahindra.com>
2020-07-28 22:29:49 +0530

    Retrofit 2 setup

---
+++ app/src/main/java/com/codingapps/newsarticle/api/RetrofitBuilder.kt
@@ -0,0 +1,21 @@
+package com.codingapps.newsarticles.api
+
+import com.codingapps.newsarticle.util.Constants
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
+object RetrofitBuilder {
+    val retrofitBuilder:Retrofit.Builder by lazy {
+        Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+    }
+
+    val apiService:ApiService by lazy {
+            retrofitBuilder
+                .build()
+                .create(ApiService::class.java)
+    }
+
+}
